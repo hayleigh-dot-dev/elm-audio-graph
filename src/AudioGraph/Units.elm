27c7410c679985c5534edde1093cd88a1d9eab65
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-01-15 17:56:22 +0000

    :sparkles: Alias parameter types for more expressive type annotations.

---
+++ src/AudioGraph/Units.elm
@@ -0,0 +1,22 @@
+module AudioGraph.Units exposing
+    ( Hertz
+    , KValue
+    , MIDI
+    )
+
+{-| -}
+
+
+{-| -}
+type alias Hertz =
+    Float
+
+
+{-| -}
+type alias KValue =
+    Float
+
+
+{-| -}
+type alias MIDI =
+    Int
