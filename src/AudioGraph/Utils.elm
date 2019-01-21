05d51eb225193b71b80234d1f2bd9a5ec0a0043a
Andrew Thompson <athom058@gold.ac.uk>
2019-01-21 23:07:03 +0000

    :art: Rename function argument names for better readability.

--- src/AudioGraph/Utils.elm
+++ src/AudioGraph/Utils.elm
@@ -6,18 +6,16 @@ module AudioGraph.Utils exposing
 
 @docs mtof, ftom
 
-@docs mtof_, ftom_
-
 -}
 
-import AudioGraph.Units exposing (..)
+import AudioGraph.Units exposing ( .. )
 
 
 {-| -}
 mtof : MIDI -> Hertz
-mtof m =
-    if m <= 0 || m < 128 then
-        (toFloat m - 69) / 12 |> (^) 2 |> (*) 440
+mtof midi =
+    if midi <= 0 || midi < 128 then
+        (toFloat midi - 69) / 12 |> (^) 2 |> (*) 440
 
     else
         0
@@ -25,5 +23,5 @@ mtof m =
 
 {-| -}
 ftom : Hertz -> MIDI
-ftom f =
-    f / 440 |> logBase 2 |> (*) 12 |> round |> (+) 69
+ftom freq =
+    freq / 440 |> logBase 2 |> (*) 12 |> round |> (+) 69
